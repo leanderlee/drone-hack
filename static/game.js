086ee5192063f6bd97ce94cbc7a6ac35e665c2c3
PPYMOU <ppymou@gmail.com>
2013-05-05 03:47:50 -0400

    Testing...

--- static/game.js
+++ static/game.js
@@ -2,11 +2,31 @@ var main = (function() {
     //private
     var canvas = undefined,
         ch = undefined, 
+        asteroids = {
+            r1: {
+                destroyed: false,
+                el: undefined,
+            },
+            r2: {
+                destroyed: false,
+                el: undefined,
+            },
+            r3: {
+                destroyed: false,
+                el: undefined,
+            }
+        },
+        asteroidsPos = {
+            r1: undefined,
+            r2: undefined,
+            r3: undefined
+        },
+        loopInterval = undefined; 
         fired = true,
-        asteroids = [],
         score = 0,
         time = 0,
-        loopInterval = undefined; 
+        canvasSizeX = 640,
+        canvasSizeY = 360;
     
     //support func
     var getCursorPosition = function(e) {
@@ -27,9 +47,61 @@ var main = (function() {
             y: y,
         }
     },
+    updateAstPos = function() {
+        for (var id in asteroidsPos) {
+            var astPos = asteroidsPos[id];
+
+            if (!astPos) {
+                asteroidsPos[id] = {};
+                asteroidsPos[id].x = Math.random()*canvasSizeX;
+                asteroidsPos[id].y = Math.random()*canvasSizeY;
+            }
+            else {
+                if (Math.random() > 0.5) {
+                    //asteroidsPos[id].x -= Math.random()*15;
+                    //asteroidsPos[id].y -= Math.random()*15;
+                }
+                else {
+                    //asteroidsPos[id].x += Math.random()*15;
+                    //asteroidsPos[id].y += Math.random()*15;
+                    asteroidsPos[id].x = 270;
+                    asteroidsPos[id].y = 150; 
+                }
+            }
+        }
+    },
     drawAst = function() {
-	var ast = $('<div>', {'class': "asteroid"});
-	canvas.append(ast);
+        for (var id in asteroids) {
+            var ast = asteroids[id],
+                astPos = asteroidsPos[id];
+             
+            if (!ast.destroyed) { //defined so has x & y
+                if (!ast.el && !ast.destroyed) {
+                    console.log('recreating...');
+                    var rockId = Math.ceil(Math.random()*5);
+                    ast.el = $('<img>', {'class': 'asteroid', 'src': 'rock'+rockId+'.png'});
+                    canvas.prepend(ast.el);
+                }
+
+                if (ast.hit) {
+                    var curId = {id: id};
+                    ast.el.attr('src', 'blast.gif');
+                    //remove itself later
+                    ast.el.animate({
+                        opactiy: 0,
+                    }, 500, function(id) {
+                        asteroids[id].destroyed = true;
+                        asteroids[id].el.remove();
+                    }(id));
+                }
+                else {
+                    ast.el.animate({
+                        top: astPos.y, 
+                        left: astPos.x, 
+                    }, 32, function() {});
+                }
+            }
+        }
     },
     fire = function(e) {
         //draw laser
@@ -37,10 +109,19 @@ var main = (function() {
         $('#laser').show();
         $('#laser').fadeOut('slow');
 
-        for (var i = 0; i < asteroids; i++) {
-            var ast = asteroids[i];
-            if (ast.css('top') > 120 && ast.css('left') > 260 &&
-                ast.css('top') < 180 && ast.css('left') < 320) {
+        for (var id in asteroids) {
+            var ast = asteroids[id];
+
+            if (!ast.el) {
+                continue;
+            }
+
+            var topCoord = parseInt(ast.el.css('top')),
+                leftCoord = parseInt(ast.el.css('left'));
+
+            if (topCoord > 120 && leftCoord > 260 &&
+                topCoord < 180 && leftCoord < 320) {
+                console.log('Woohoo~');
                 ast.hit = true;
                 score += 100; //change 
             }
@@ -50,6 +131,7 @@ var main = (function() {
     //setting up game
     var mainLoop = function() {
         console.log("loop");
+        updateAstPos();
         drawAst();
         time += 0.032;
     },
@@ -58,13 +140,13 @@ var main = (function() {
         canvas.click(fire);
         ch = $('#crossHair');
 
-        loopInterval = setIntervasetInterval(mainLoop,32); //30fps
+        loopInterval = setInterval(mainLoop,32); //30fps
     },
     finish = function() { //returns final game score
         clearInterval(loopInterval); 
         return score/time;
     },
-    getStats = function {
+    getStats = function () {
         return {
         };
     };
@@ -81,4 +163,8 @@ var main = (function() {
 $(function() {
     //Testing. Draw red square, listen for click event, react
     main.init();
+    var t = $('<img>', {'class': 'asteroid', 'src': 'rock3.png'});
+    $('#canvas').prepend(t);
+    t.css('top',170);
+    t.css('left', 310);
 });
